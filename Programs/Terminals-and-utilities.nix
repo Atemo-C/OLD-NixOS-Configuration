ff00f690ab93cd7e08c792e4bf4a7ede6c2b55d9
Atemo Cajaku <160250128+Atemo-C@users.noreply.github.com>
2024-05-08 15:43:51 +0000

    Create `Terminals-and-utilities.nix`

---
+++ Programs/Terminals-and-utilities.nix
@@ -0,0 +1,18 @@
+{ config, lib, pkgs, ... }: {
+
+	environment.systemPackages = with pkgs; [
+
+		# Terminal emulators
+		alacritty
+		lxterminal
+
+		# Various terminal utilities and eessentials
+		bat
+		calcurse
+		devour
+		dash
+		wget
+		git
+	];
+
+}
