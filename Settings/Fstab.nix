c7f5ed4b88fb084f4044a6bc9a6c1364511570c8
Atemo Cajaku <160250128+Atemo-C@users.noreply.github.com>
2024-05-08 15:51:14 +0000

    Create `Fstab.nix`

---
+++ Settings/Fstab.nix
@@ -0,0 +1,14 @@
+{ config, lib, pkgs, ... }: {
+
+#	fileSystems."/where/to/mount/" = {
+#		device = "/dev/disk/which/one/to/mount";
+#		fsType = "btrfs-or-something";
+#		options = [
+#			"defaults"
+#			"rw"
+#			"nofail"
+#			"users"
+#		];
+#	};
+
+}
