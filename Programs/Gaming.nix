7d784be1373d9e8b3d152f68e1bf1c81f680152d
Atemo Cajaku <160250128+Atemo-C@users.noreply.github.com>
2024-05-08 15:40:32 +0000

    Create `Gaming.nix`

---
+++ Programs/Gaming.nix
@@ -0,0 +1,51 @@
+{ config, lib, pkgs, ... }: {
+
+	environment.systemPackages = with pkgs; [
+		# Games and emulators
+		desmume
+		unstable.minetest
+		superTuxKart
+
+		# Game launchers
+		prismlauncher
+
+		# Gaming utilities
+		unstable.gamescope
+		ferium
+		jstest-gtk
+
+		# Java
+		jdk
+	];
+
+	boot.kernel.sysctl = { "vm.max_map_count" = 2147483642; };
+
+	programs = {
+		gamemode = {
+			enable = true;
+			settings.general = { inhibit_screensaver = 0; };
+		};
+		steam = {
+			dedicatedServer.openFirewall = false;
+			remotePlay.openFirewall = false;
+			enable = true;
+		};
+	};
+
+	# Mostly for emulators
+	security.pam.loginLimits = [
+		{
+			domain = "*";
+			type = "hard";
+			item = "memlock";
+			value = "unlimited";
+		}
+		{
+			domain = "*";
+			type = "soft";
+			item = "memlock";
+			value = "unlimited";
+		}
+	];
+
+}
