b9aa9a6b47ad6099d7c2b98bdc965ecb4477f636
Atemo Cajaku <160250128+Atemo-C@users.noreply.github.com>
2024-05-08 15:23:57 +0000

    Create `NVIDIA-GPU.nix`

---
+++ Drivers/NVIDIA-GPU.nix
@@ -0,0 +1,38 @@
+{ config, lib, pkgs, ... }: {
+
+	boot = {
+		extraModulePackages = [ config.boot.kernelPackages.nvidia_x11 ];
+		initrd.kernelModules = [ "nvidia" ];
+		kernelParams = [ "nvidia.NVreg_PreserveVideoMemoryAllocations=1" ];
+	};
+
+	environment = {
+		sessionVariables = { WLR_NO_HARDWARE_CURSORS = "1"; };
+		systemPackages = with pkgs; [
+			linuxPackages.nvidia_x11
+#			cudaPackages.cudatoolkit
+		];
+	};
+
+	hardware.nvidia = {
+		modesetting.enable = true;
+		nvidiaSettings = true;
+		powerManagement.enable = true;
+#		prime = { # NVIDIA PRIME is untested.
+#			amdgpuBusId = "PCI:42:0:0";
+#			nvidiaBusId = "PCI:16:0:0";
+#			offload = {
+#				enable = true;
+#				enableOffloadCmd = true;
+#			};
+#		};
+	};
+
+	services.xserver.videoDrivers = [
+		"nvidia"
+#		"nvidialegacy470"
+#		"nvidialegacy390"
+#		"nvidialegacy340"
+	];
+
+}
