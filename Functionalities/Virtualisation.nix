5bd9b401c264f183a048910982528cc8455ff2d2
Atemo Cajaku <160250128+Atemo-C@users.noreply.github.com>
2024-05-08 15:36:00 +0000

    Create `Virtualisation.nix`

---
+++ Functionalities/Virtualisation.nix
@@ -0,0 +1,30 @@
+{ config, lib, pkgs, ... }: {
+
+	programs.virt-manager.enable = true;
+
+	home-manager.users.atemo.dconf.settings = {
+		"org/virt-manager/virt-manager/connections" = {
+			autoconnect = [ "qemu:///system" ];
+			uris = [ "qemu:///system" ];
+		};
+	};
+
+	virtualisation = {
+		libvirtd.enable = true;
+		virtualbox.host = { # Warning: Takes a long time to compile.
+			enable = true;
+			enableExtensionPack = true;
+			package = pkgs.unstable.virtualbox;
+		};
+#		docker = {
+#			enable = true;
+#			rootless = {
+#				enable = true;
+#				setSocketVariable = true;
+#			};
+#		};
+	};
+
+	users.extraGroups.vboxusers.members = [ "atemo" ];
+
+}
