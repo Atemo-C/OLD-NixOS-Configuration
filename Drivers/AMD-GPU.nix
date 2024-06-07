e4e25be0e8179a31156cf6c7f0bd83d2fe716954
Atemo-C <c.roux1993@gmail.com>
2024-06-07 22:21:39 +0200

    Re-worked the `AMD-GPU.nix` module

--- Drivers/AMD-GPU.nix
+++ Drivers/AMD-GPU.nix
@@ -1,7 +1,23 @@
 { config, lib, pkgs, ... }: {
 
 	boot.initrd.kernelModules = [ "amd" ];
-	hardware.opengl.extraPackages = with pkgs; [ rocmPackages.clr.icd ];
+
+#	environment.variables = {
+#		AMD_VULKAN_ICD = "RADV";
+#		VK_ICD_FILENAMES = "/run/opengl-driver/share/vulkan/icd.d/radeon_icd.x86_64.json";
+#		ROC_ENABLE_PRE_VEGA = "1";
+#	};
+
+	hardware.opengl = {
+		extraPackages = with pkgs; [
+			rocmPackages.clr.icd
+#			amdvlk
+		];
+#		extraPackages32 = with pkgs; [
+#			driversi686Linux.amdvlk
+#		];
+	};
+
 	services.xserver.videoDrivers = [ "amdgpu" ];
 
 }
